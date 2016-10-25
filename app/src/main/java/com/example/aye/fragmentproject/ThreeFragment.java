5d314a56754a95fa9360bfae9cdcff00b3d3d700
muzakki <ayecartoon@gmail.com>
2016-10-26 00:07:16 +0700

    upload project

---
+++ app/src/main/java/com/example/aye/fragmentproject/ThreeFragment.java
@@ -0,0 +1,29 @@
+package com.example.aye.fragmentproject;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.aye.fragmentproject.R;
+
+/**
+ * Created by aye on 10/25/2016.
+ */
+
+public class ThreeFragment extends Fragment {
+    public ThreeFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_three, container, false);
+    }
+
+}
